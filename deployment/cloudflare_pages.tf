907d6d9b28f0a42407d2b4fb82ae8348edaa74d8
Matt Newman <mdjnewman@gmail.com>
2024-05-03 11:59:05 -0500

    Create new Pages project with no git integration

--- deployment/cloudflare_pages.tf
+++ deployment/cloudflare_pages.tf
@@ -36,3 +36,10 @@ resource "cloudflare_pages_project" "dunningnatural" {
     }
   }
 }
+
+
+resource "cloudflare_pages_project" "dunningnatural-pages" {
+  account_id        = cloudflare_account.dunningnatural.id
+  name              = "dunningnatural-pages"
+  production_branch = "main"
+}
