98978f230a7183fa8b6342123fc754fafc635136
Matt Newman <mdjnewman@gmail.com>
2024-05-03 09:41:59 -0500

    Add in Cloudflare secrets & deploy step #10
    Likely need more IAM permissions for runner pool

---
+++ scripts/fetch_secrets.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o noglob
+
+CLOUDFLARE_API_TOKEN=$(gcloud \
+    --project=dunningnatural-3e6e829d \
+    secrets versions access current \
+    --secret=cloudflare_api_token)
+
+CLOUDFLARE_API_TOKEN=foo
+
+echo "CLOUDFLARE_API_TOKEN=${CLOUDFLARE_API_TOKEN}" >> "$GITHUB_ENV"
